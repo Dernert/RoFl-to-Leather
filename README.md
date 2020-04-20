212edb4666399baad66ccdd8e23d48ef70c71af2
Dernert <dernert@outlook.com>
2020-04-20 12:14:24 +0200

    add project via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# RoFl-to-Leather
+## RoFl2Leather
+
+RoFl2Leather adds a new Recipe.
+Cook Rotten Flesh on a Campfire to get Leather.
---
+++ src/main/java/de/nertgaming/rofl2leather/Main.java
@@ -0,0 +1,9 @@
+package de.nertgaming.rofl2leather;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("rofl2leather")
+public class Main {
+
+
+}
