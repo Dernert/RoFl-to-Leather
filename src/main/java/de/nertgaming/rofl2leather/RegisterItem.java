fed829855314287fde2741ccff4ae9081838f9b6
dernert <dernert@outlook.com>
2020-05-01 15:36:03 +0200

    reupload

--- src/main/java/de/nertgaming/rofl2leather/RegisterItem.java
+++ src/main/java/de/nertgaming/rofl2leather/RegisterItem.java
@@ -2,6 +2,7 @@ package de.nertgaming.rofl2leather;
 
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemTier;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -9,12 +10,19 @@ import net.minecraftforge.registries.ForgeRegistries;
 
 public class RegisterItem {
 
-    //The ITEMS deferred register in which you can register items.
     public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, Main.MODID);
 
-    //Register the tutorial dust with "tutorial_dust" as registry name and default properties
     public static final RegistryObject<Item> dried_flesh = ITEMS.register("dried_flesh",
             () -> new Item(new Item.Properties().group(ItemGroup.MISC)));
+
+    public static final RegistryObject<Item> skin = ITEMS.register("skin",
+            () -> new Item(new Item.Properties().group(ItemGroup.MISC)));
+
+    public static final RegistryObject<Item> hunting_knife_bone = ITEMS.register("hunting_knife_bone",
+            () -> new ItemHuntingKnife(ItemTier.IRON, 0, -2.2f, new Item.Properties().group(ItemGroup.COMBAT)));
+
+    public static final RegistryObject<Item> hunting_knife_wood = ITEMS.register("hunting_knife_wood",
+            () -> new ItemHuntingKnife(ItemTier.WOOD, 1, -2.2f, new Item.Properties().group(ItemGroup.COMBAT)));
 
     public RegisterItem(){
         ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
