b84e8e87f0e7c7f1d8c42f3388976beb5f995883
dernert <dernert@outlook.com>
2021-04-09 14:06:25 +0200

    update to 1.16.5

--- src/main/java/de/nertgaming/rofl2leather/RegisterItem.java
+++ src/main/java/de/nertgaming/rofl2leather/RegisterItem.java
@@ -13,16 +13,16 @@ public class RegisterItem {
     private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Main.MODID);
 
     public static final RegistryObject<Item> dried_flesh = ITEMS.register("dried_flesh",
-            () -> new Item(new Item.Properties().group(ItemGroup.MISC)));
+            () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MISC)));
 
     public static final RegistryObject<Item> skin = ITEMS.register("skin",
-            () -> new Item(new Item.Properties().group(ItemGroup.MISC)));
+            () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MISC)));
 
     public static final RegistryObject<Item> hunting_knife_bone = ITEMS.register("hunting_knife_bone",
-            () -> new ItemHuntingKnife(ItemTier.IRON, 0, -2.2f, new Item.Properties().group(ItemGroup.COMBAT)));
+            () -> new ItemHuntingKnife(ItemTier.IRON, 0, -2.2f, new Item.Properties().tab(ItemGroup.TAB_COMBAT)));
 
     public static final RegistryObject<Item> hunting_knife_wood = ITEMS.register("hunting_knife_wood",
-            () -> new ItemHuntingKnife(ItemTier.WOOD, 1, -2.2f, new Item.Properties().group(ItemGroup.COMBAT)));
+            () -> new ItemHuntingKnife(ItemTier.WOOD, 1, -2.2f, new Item.Properties().tab(ItemGroup.TAB_COMBAT)));
 
     public RegisterItem(){
         ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
